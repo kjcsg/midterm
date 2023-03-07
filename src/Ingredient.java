b74a9fb376d86f83cf3f0a5006861c844e98b6bc
Kyle Comsa (Laptop) <kjcsg17@gmail.com>
2023-03-06 16:52:13 -0800

    Midterm init commit

---
+++ src/Ingredient.java
@@ -0,0 +1,9 @@
+public class Ingredient {
+    public String name;
+    public int amount;
+
+    Ingredient(String name, int amount){ //Takes the input name and amount and makes them one ingredient object
+       this.name = name;
+       this.amount = amount;
+    }
+}
