3dacd4667b32f6419ca9bfd38a15e5e1f83d9a53
Kyle Comsa (Laptop) <kjcsg17@gmail.com>
2023-03-06 20:22:25 -0800

    Final version

--- src/Midterm.java
+++ src/Midterm.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Objects;
 
 public class Midterm {
     private static Pantry wegot = new Pantry(); //Create instance of pantry with label wegot b/c it's what we got
     public static void main(String[] args){
         buildPantry(); //Build up the pantry list
+        recipeCheck(); //Check all the recipes in the folder, see if we can make them
     }
+
     private static void buildPantry() { //Build up the pantry list function
-        String name;
+        String item;
         float amount;
 
         try {
@@ -17,15 +18,49 @@ public class Midterm {
             Scanner scan = new Scanner(list).useDelimiter(",|\r\n");
 
             while(scan.hasNext()) {
-                name = scan.next();
+                item = scan.next();
                 amount = scan.nextFloat();
-                wegot.add(name,amount);
+                wegot.add(item,amount); //Add each item and quantity 
             }
-            scan.close();
         }
         catch(FileNotFoundException e) {
             System.out.println("No file found.");
-            e.printStackTrace();
+        }
+    }
+
+    private static void recipeCheck() { //Check if we can make the recipe with what we got
+        String name; //Name of recipe
+        String item; //Name of item needed
+        float amount; //Number of item we have
+        float needed; //Number of item we need
+        boolean fileExists = true;
+        int i = 1; //Start at recipe number 1
+        
+        while(fileExists){ //As long as the file exists, go through it and check if we have the ingredients needed
+            try {
+                File list = new File("Recipe"+i+".txt"); //State which recipe we're looking for by recipe number
+                i++; //Increase recipe number for next run
+                Scanner scan = new Scanner(list).useDelimiter(",|\r\n"); //Set up scanner to read recipe
+                name = scan.nextLine(); //Grab the recipe name from the top of the file
+                boolean canMake = true;
+                while(scan.hasNext()){ //Go through each item needed in the recipe
+                        item = scan.next(); //Which item are we in need of
+                        needed = scan.nextFloat(); //How much of the item do we need
+                        amount = wegot.getQuantity(item); //Find out how much of the item we have
+                        if(amount<needed){ //If we don't have enough of something, print out that we don't have enough to make this recipe
+                            System.out.println("We don't have enough ingredients to make "+name+".");
+                            canMake = false;
+                            scan.close();
+                            break; //Don't bother going through the rest of the recipe that can't be made
+                        }
+                }
+                if(canMake){System.out.println("We can make "+name+"!");} //Hooray! We can make this recipe
+                scan.close();
+            }
+            catch(FileNotFoundException e) {
+                System.out.println("Reached end of recipes."); //Let us know we've gone through all the recipes in the folder
+                fileExists = false; //Bring this program to a close
+            }
         }
     }
 }
--- src/Pantry.java
+++ src/Pantry.java
@@ -2,52 +2,21 @@ import java.util.Objects;
 import java.util.LinkedList;
 
 public class Pantry {
-    private static LinkedList ingredients = new LinkedList<>();
+    private static LinkedList<Ingredient> ingredients = new LinkedList<Ingredient>(); //Set up empty ingredients list
 
-    public static void add(String name, float quantity){
-        Ingredient item = new Ingredient(name, quantity);
-        ingredients.add(item); //Adds ingredients to the pantry ingredients list
-        //System.out.println(ingredients.size());
-        for(int i=0; i<ingredients.size(); i++){
-            //Ingredient test = ingredients.get(i);
-        }
+    public static void add(String name, float quantity){ //Take a name and amount...
+        Ingredient item = new Ingredient(name, quantity);//...make the name and amount into an ingredient object...
+        ingredients.add(item); //...add ingredient object to the pantry ingredients list
     }
 
-    public static int getQuantity(String name){
-        for(int i=0; i<ingredients.size(); i++){
-            Ingredient item = ingredients.get(i);
-            if(ingredients.get(i).name==name){
-
+    public static float getQuantity(String name){ //Find out how much of an ingredient we have
+        float quantity = 0; //Default to zero
+        for(int i=0; i<ingredients.size(); i++){ //Go through each ingredient in the pantry
+            if(Objects.equals(ingredients.get(i).name, name)){
+                quantity = ingredients.get(i).amount; //If we have the ingredient, return how much we have
+                break; //Don't bother going through the rest of the ingredients in the pantry
             }
         }
+        return quantity;
     }
-
-    public static int length(){ //Returns base 1 length
-        return ingredients.size();
-    }
-/*
-    public static boolean has(String name){ //Retruns true if the name is in the queue
-        boolean inqueue = false;
-        for (int i=0; i<queue.length; i++){
-            if(Objects.equals(queue[i].name.toLowerCase(), name.toLowerCase())){
-                inqueue = true;
-                break;
-            }
-        }
-        return inqueue;
-    }
-
-    public static void show(){ //This is just for troubleshooting purposes, will remove before turning in. Prints out the current queue
-        int i = 0; 
-        int qL = queue.length;
-
-        if(queue[i]==null){System.out.println("The queue is empty.");}
-        else{
-            System.out.println("Current Queue:");
-            while(i<queue.length){
-                System.out.println(i+1 + " " + queue[i].name + ", " + queue[i].reason + ", " + queue[i].time);
-                i++;
-            }
-        }        
-    }*/
 }
